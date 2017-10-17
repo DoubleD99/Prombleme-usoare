07c905fca72e305be98c72352036b452c7625e0f
DoubleD99 <32884730+DoubleD99@users.noreply.github.com>
2017-10-18 00:43:13 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("a=");
+    scanf ("%d",&a);
+    if(a>10)
+    printf("a>10");
+    if(a<10)
+    printf("a<10");
+    if(a==10)
+    printf("a==10");
+
+    getchar();
+
+    return 0;
+
+
+
+
+
+
+
+}
