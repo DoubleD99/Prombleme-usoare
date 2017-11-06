dcc0171b78b1a0d0146674779facb4b1318900f9
DoubleD99 <32884730+DoubleD99@users.noreply.github.com>
2017-11-07 00:18:27 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,24 +1,17 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int a;
-    printf("a=");
-    scanf ("%d",&a);
-    if(a>10)
-    printf("a>10");
-    if(a<10)
-    printf("a<10");
-    if(a==10)
-    printf("a==10");
-
-    getchar();
-
+    int a, b;
+    printf("Introduceti valoarea lui a=");
+    scanf("%d", &a);
+    printf("Introduceti val lui b=");
+    scanf("%d", &b);
+    if(a>b)
+        printf("Numarul mai mare este %d" , a);
+    else
+        if(a<b)
+        printf("Numarul mai mare este %d", b);
+    else
+    printf("Numerele sunt egale");
     return 0;
-
-
-
-
-
-
-
 }
