2a860a6efa0f39a2d777f9b17e1e1ec4a4f13c86
DoubleD99 <32884730+DoubleD99@users.noreply.github.com>
2017-11-07 00:40:47 +0200

    Add files via upload

---
+++ 3numere.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Introduceti valoarea lui a=");
+    scanf("%d", &a);
+    printf("Introduceti val lui b=");
+    scanf("%d", &b);
+    printf("Introduceti valoarea lui c=");
+    scanf("%d", &c);
+    if(a>b &&b>c)
+        printf("Numarul mai mare este %d" , a);
+    else
+        if(a<b &&b<c)
+        printf("Numarul mai mare este %d", c);
+    else
+    if(a<b &&c<b)
+    printf("Numerul mai mare este %d", b);
+    else
+        if(a>b &&b<c)
+        printf("Numarul mai mare este %d", a);
+
+    return 0;
+}
